ab7f24e20e236324cf4d96e9d6e4d6591d25f5f2
fcolobe <fontycolobe@icloud.com>
2024-06-14 18:04:27 +0200

    Add youtube_channel_stats dataset
    Exported youtube_channel_stats dataset using use_data().
    Documented the dataset in data.R.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' YouTube Channel Statistics
+#'
+#' This dataset contains statistics of YouTube channels sourced from Kaggle.
+#' 
+#' The data includes various metrics such as rank, subscribers, video views,
+#' category, country, earnings, and more for the top YouTube channels.
+#' 
+#' Source: [Kaggle - Global YouTube Statistics 2023](https://www.kaggle.com/datasets/nelgiriyewithana/global-youtube-statistics-2023/data)
+#' 
+#' @format A data frame with statistics of YouTube channels.
+#' @keywords datasets
+"youtube_channel_stats"
